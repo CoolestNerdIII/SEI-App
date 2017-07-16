05b49bfee041197adb4567e4c13b4195ab5fd831
Dominique Paster <dpaster1@gmail.com>
2017-07-16 16:06:32 -0400

    Initial Commit

---
+++ SEI App/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SEI App
+//
+//  Created by Dominique Paster on 7/16/17.
+//  Copyright © 2017 CDP Development Solutions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
